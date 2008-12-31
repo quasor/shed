91dd1cf7892429dc71ed9df5b4ec485ce6518271
Andrew Coldham <andrewcoldham@mac.com>
2008-12-31 11:35:44 -0800

    change caching to memcache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,7 @@ config.cache_classes = true
 # Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
+config.action_view.cache_template_loading            = true
 
 # Use a different cache store in production
 # config.cache_store = :mem_cache_store
@@ -22,3 +23,7 @@ config.action_controller.perform_caching             = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
+
+config.action_controller.session_store = :mem_cache_store
+config.action_controller.cache_store = :mem_cache_store
+config.cache_store = :mem_cache_store, 'localhost:11211', :namespace => 'schedule'
